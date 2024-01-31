a73257993e9a02b696818bd481cb2672a7e4caa8
Lívia Carolina <liviacarolinasouzasicchieri@gmail.com>
2024-01-31 13:28:50 -0300

    Enviando o arquivo

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const server = express();
+const port: number = 3000;
+
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.json('Olá');
+})
+
+server.listen(port, () => [
+    console.log(`http://localhost:${port}`)
+])

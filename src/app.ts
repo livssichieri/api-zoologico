aa1598e9c16b57d286646bd3a0ea877db5e830f4
Lívia Carolina <liviacarolinasouzasicchieri@gmail.com>
2024-02-28 14:41:53 -0300

    Atualizando código #2

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { Habitat } from "./model/Habitat";
-import { Atracao } from "./model/Atracao";
-import { Zoologico } from "./model/Zoologico";
 import { DatabaseModel } from "./model/DatabaseModel";
 import { Reptil } from "./model/Reptil";
 import { Mamifero } from "./model/Mamifero";
 import { Ave } from "./model/Ave";
-import { getEnabledCategories } from "trace_events";
 
 const server = express();
 const port: number = 3000;
@@ -17,27 +13,6 @@ server.use(cors());
 
 server.get('/', (req, res) => {
     res.json("ola");
-});
-
-server.post('/habitat', (req, res) => {
-    const { nome, animais } = req.body;
-    const habitat = new Habitat(nome, animais);
-    console.log(habitat);
-    res.status(200).json('Habitat criado');
-});
-
-server.post('/atracao', (req, res) => {
-    const { nome, habitat } = req.body;
-    const atracao = new Atracao(nome, habitat);
-    console.log(atracao);
-    res.status(200).json('Atração criada');
-});
-
-server.post('/zoologico', (req, res) => {
-    const { nome, atracao } = req.body;
-    const zoo = new Zoologico(nome, atracao);
-    console.log(zoo);
-    res.status(200).json('Zoológico criado');
 });
 
 server.get('/list/reptil', async (req, res) => {
